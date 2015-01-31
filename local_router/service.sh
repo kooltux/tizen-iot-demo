fe231912cc9a92b41a590a9eb1ad3b9fccf37498
Stephane Desneux <stephane.desneux@open.eurogiciel.org>
2015-01-31 18:32:38 +0100

    setup server
    Change-Id: I74a8e5f8f0b969827f007dc16168a13034130969
    Signed-off-by: Stephane Desneux <stephane.desneux@open.eurogiciel.org>

--- local_router/service.sh
+++ local_router/service.sh
@@ -9,6 +9,6 @@ cat /dev/zero >/dev/fb1
 trap "echo 'Releasing rfcomm...';rfcomm release $IOT_COMM_ROUTER; killall rfcomm >/dev/null 2>&1" STOP INT QUIT EXIT
 
 DIR=$(cd $(dirname $0) && pwd -P)
-rfcomm -r watch $IOT_COMM_ROUTER 1 $DIR/event_router {} $IOT_COMM_SERVER $IOT_SERVER_BT
+rfcomm -r watch $IOT_COMM_ROUTER 1 $DIR/event_router {} $IOT_COMM_SERVER $IOT_SERVER_BT 2
 
 
--- remote_server/service.sh
+++ remote_server/service.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-rfkill unblock all
-
-. /etc/iot/network.conf
-
-trap "echo 'Releasing rfcomm...';rfcomm release $IOT_COMM_ROUTER; killall rfcomm >/dev/null 2>&1" STOP INT QUIT EXIT
-
 DIR=$(cd $(dirname $0) && pwd -P)
-rfcomm -r watch $IOT_COMM_SERVER 1 $DIR/post_event {} http://localhost:8080/event
+echo "Starting auto-yes-agent"
+killall auto-yes-agent
+PYTHONPATH=/usr/lib64/bluez/test/ $DIR/auto-yes-agent &
+
+sleep 1
+
+echo "Running as root..." >&2
+exec sudo $DIR/service_root.sh
 
